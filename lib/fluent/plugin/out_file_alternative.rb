6a74256365faad6bf9c40334d5053230ee78b67f
TAGOMORI Satoshi <tagomoris@gmail.com>
2012-03-16 12:15:56 +0900

    write plugin frame

--- lib/fluent/plugin/out_file_alternative.rb
+++ lib/fluent/plugin/out_file_alternative.rb
@@ -1,4 +1,38 @@
-module Fluent
-  class FileAlternativeOutput < Fluent::TimeSlicedOutput
+class Fluent::FileAlternativeOutput < Fluent::TimeSlicedOutput
+  Fluent::Plugin.register_output('file_alternative', self)
+
+  config_set_default :buffer_type, 'memory'
+  # config_param :hoge, :string, :default => 'hoge'
+
+  def initialize
+    super
+    # require 'hogepos'
+  end
+
+  def configure(conf)
+    super
+    # @path = conf['path']
+  end
+
+  def start
+    super
+    # init
+  end
+
+  def shutdown
+    super
+    # destroy
+  end
+
+  def format(tag, time, record)
+    [tag, time, record].to_msgpack
+  end
+
+  def write(chunk)
+    records = []
+    chunk.msgpack_each { |record|
+      # records << record
+    }
+    # write records
   end
 end
